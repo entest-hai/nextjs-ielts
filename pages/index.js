4c3895a2b144d1aea9639d3ad4695fb0a8d0201e
Tran Minh Hai <hai@bio-rithm.com>
2021-11-20 19:17:25 +0700

    update

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function TestMaterialUI() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+export default function Home() {
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <TestMaterialUI></TestMaterialUI>
+    </ThemeProvider>
+  )
+}
